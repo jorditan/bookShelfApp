36d7d5dcb0557c2782067e3cb5cf8d2d96b4d329
Matias Owen Jordan <matiasowjordan@gmail.com>
2025-12-16 08:31:17 -0300

    adding handle

---
+++ internal/transport/book_handler.go
@@ -0,0 +1,53 @@
+package transport
+
+import (
+	"encoding/json"
+	"main/internal/model"
+	"main/internal/service"
+	"net/http"
+)
+
+type BookHandler struct {
+	service *service.Service
+}
+
+func New(s *service.Service) *BookHandler {
+	return &BookHandler{
+		service: s,
+	}
+}
+
+func (h *BookHandler) HandleBooks(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		libros, err := h.service.ObtenTodosLosLibros()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(libros)
+
+	case http.MethodPost:
+		var libro model.Libro
+		if err := json.NewDecoder(r.Body).Decode(&libro); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		created, err := h.service.CrearLibro(libro)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(created)
+
+	default:
+		http.Error(w, "Método no disponible", http.StatusMethodNotAllowed)
+	}
+}
