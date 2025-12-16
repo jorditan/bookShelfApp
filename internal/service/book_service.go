46e2c893c429bdba63a0da4d0fab120fe7a5c47d
Matias Owen Jordan <matiasowjordan@gmail.com>
2025-12-16 08:17:06 -0300

    adding logger

--- internal/service/book_service.go
+++ internal/service/book_service.go
@@ -1,20 +1,37 @@
 package service
 
 import (
+	"errors"
 	"main/internal/model"
 	"main/internal/store"
 )
 
+type Logger interface {
+	Log(msg, error string)
+}
+
 type Service struct {
-	store store.Store
+	store  store.Store
+	logger Logger
 }
 
 func New(s store.Store) *Service {
-	return &Service{store: s}
+	return &Service{
+		store:  s,
+		logger: nil,
+	}
 }
 
 func (s *Service) ObtenTodosLosLibros() ([]*model.Libro, error) {
-	return s.store.GetAll()
+	s.logger.Log("Estamos obteniendo los libros", "")
+
+	libros, err := s.store.GetAll()
+
+	if err != nil {
+		s.logger.Log("El error es %v\n", err.Error())
+		return nil, err
+	}
+	return libros, nil
 }
 
 func (s *Service) ObtenLibroPorId(id int) (*model.Libro, error) {
@@ -22,5 +39,19 @@ func (s *Service) ObtenLibroPorId(id int) (*model.Libro, error) {
 }
 
 func (s *Service) CrearLibro(libro model.Libro) (*model.Libro, error) {
+	if libro.Titulo == "" {
+		return nil, errors.New("necesitamos el título")
+	}
 	return s.store.Create(&libro)
+}
+
+func (s *Service) UpdateAlLibro(id int, libro model.Libro) (*model.Libro, error) {
+	if libro.Titulo == "" {
+		return nil, errors.New("necesitamos el título")
+	}
+	return s.store.Update(id, &libro)
+}
+
+func (s *Service) RemoverLibro(id int) error {
+	return s.store.Delete(id)
 }
